c3644a8bdef573fc3c65be86efd8d42a10b01eff
knutsvk <knut@spacemaker.ai>
2021-12-01 23:32:32 +0100

    day1 two-liner

--- 2021/day1.py
+++ 2021/day1.py
@@ -1,9 +1,5 @@
 import numpy as np
 
 if __name__ == "__main__":
-    depths = np.loadtxt("input1")
-    diffs = depths[1:] - depths[:-1]
-    print(sum(diffs > 0))
-    windows = depths[:-2] + depths[1:-1] + depths[2:]
-    diffs = windows[1:] - windows[:-1]
-    print(sum(diffs > 0))
+    d = np.loadtxt("input1")
+    print(sum(d[1:] > d[:-1]), sum(d[3:] > d[:-3]))
