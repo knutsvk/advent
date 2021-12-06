8b0025b06945e55d440851a401ab3db9b9767ea5
knutsvk <knut@spacemaker.ai>
2021-12-06 10:15:01 +0100

    minimalistic day 6

--- 2021/day6.py
+++ 2021/day6.py
@@ -1,14 +1,13 @@
 with open("input6") as f:
-    x = [int(x) for x in f.readline().split(",")]
-    fish = {age: len([a for a in x if a == age]) for age in range(9)}
+    x = [int(c) for c in f.readline().split(",")]
 
-for d in range(256):
-    if d == 80:
-        print(sum(x for x in fish.values()))
-    new_fish = {}
-    for a in range(7, -1, -1):
-        new_fish[a] = fish[a + 1]
-    new_fish[8] = fish[0]
-    new_fish[6] += fish[0]
-    fish = new_fish
-print(sum(x for x in fish.values()))
+
+def count_fish(n):
+    fish = [len([a for a in x if a == age]) for age in range(9)]
+    for d in range(n):
+        fish.append(fish.pop(0))
+        fish[6] += fish[8]
+    return sum(fish)
+
+
+print(count_fish(80), count_fish(256))
